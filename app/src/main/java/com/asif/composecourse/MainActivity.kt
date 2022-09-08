baa1adcbc2cd813de5d933977078ece6ada69c70
asifj96 <masifj1996@gmail.com>
2022-09-09 00:03:13 +0500

    ImageCard

--- app/src/main/java/com/asif/composecourse/MainActivity.kt
+++ app/src/main/java/com/asif/composecourse/MainActivity.kt
@@ -3,43 +3,141 @@ package com.asif.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
+            /* Row(
+                 modifier = Modifier
+                     .width(200.dp)
+                     .height(200.dp)
+                     .background(Color.Green),
+                 horizontalArrangement = Arrangement.SpaceAround,
+                 verticalAlignment = Alignment.CenterVertically
+             ) {
+                 AddText(name = "Asif")
+                 AddText(name = "Javed")
+                 AddText(name = "Awan")
+             }*/
+
+            val painter = painterResource(id = R.drawable.android_13)
+            val description = "Image Card "
+            val title = "Image Card "
+
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
+                    .fillMaxWidth(0.6f)
+                    .padding(16.dp)
             ) {
-                Greeting(name = "Welcome To ComposeCourse")
+                ImageCard(
+                    painter = painter,
+                    contentDescription = description,
+                    title = title
+                )
             }
         }
+    }   
+}
+
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+
+            Image(
+                modifier = modifier.clickable { },
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+
+        }
     }
+
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(
-        text = "$name!"
-    )
+    Text(text = "Hello $name!")
+}
+
+@Composable
+fun AddText(name: String) {
+    Text(text = name, Modifier.clickable {
+
+    })
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    val painter = painterResource(id = R.drawable.android_13)
+    val description = "Image Card "
+    val title = "Image Card "
 
-    Greeting(name = "Welcome To ComposeCourse")
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(0.6f)
+            .padding(16.dp)
+    ) {
+        ImageCard(
+            painter = painter,
+            contentDescription = description,
+            title = title
+        )
+    }
 }
