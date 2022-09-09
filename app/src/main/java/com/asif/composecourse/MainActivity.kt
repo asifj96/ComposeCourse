0b774052194c363cc6930ea845de9b8f5c621e1c
STCProject2022 <asifoffical429@gmail.com>
2022-09-09 16:01:04 +0500

    Styling Text

--- app/src/main/java/com/asif/composecourse/MainActivity.kt
+++ app/src/main/java/com/asif/composecourse/MainActivity.kt
@@ -3,141 +3,70 @@ package com.asif.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            /* Row(
-                 modifier = Modifier
-                     .width(200.dp)
-                     .height(200.dp)
-                     .background(Color.Green),
-                 horizontalArrangement = Arrangement.SpaceAround,
-                 verticalAlignment = Alignment.CenterVertically
-             ) {
-                 AddText(name = "Asif")
-                 AddText(name = "Javed")
-                 AddText(name = "Awan")
-             }*/
 
-            val painter = painterResource(id = R.drawable.android_13)
-            val description = "Image Card "
-            val title = "Image Card "
+        val fontFamily = FontFamily(
+            Font(R.font.lexend_bold, FontWeight.Thin),
+        )
+        setContent {
 
             Box(
                 modifier = Modifier
-                    .fillMaxWidth(0.6f)
-                    .padding(16.dp)
+                    .fillMaxSize()
+                    .background(Color(0xFF101010))
             ) {
-                ImageCard(
-                    painter = painter,
-                    contentDescription = description,
-                    title = title
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("J")
+                        }
+                        append("etpack ")
+
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("C")
+                        }
+                        append("ompose")
+
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline,
                 )
             }
-        }
-    }   
-}
-
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-
-    Card(
-        modifier = modifier
-            .fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-
-            Image(
-                modifier = modifier.clickable { },
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 300f
-                        )
-                    )
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomCenter
-            ) {
-                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-            }
 
         }
-    }
-
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Composable
-fun AddText(name: String) {
-    Text(text = name, Modifier.clickable {
-
-    })
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    val painter = painterResource(id = R.drawable.android_13)
-    val description = "Image Card "
-    val title = "Image Card "
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth(0.6f)
-            .padding(16.dp)
-    ) {
-        ImageCard(
-            painter = painter,
-            contentDescription = description,
-            title = title
-        )
     }
 }
